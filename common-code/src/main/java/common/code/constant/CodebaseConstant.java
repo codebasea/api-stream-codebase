067ba23baf7fd09570e3a59a6c5ac64cd7bef966
codebasea <110478608+codebasea@users.noreply.github.com>
2023-09-07 16:05:05 +0530

    remove all code

--- common-code/src/main/java/common/code/constant/CodebaseConstant.java
+++
@@ -1,7 +0,0 @@
-package common.code.constant;
-
-public class CodebaseConstant {
-    public static final String SIMPLE_DATE_FORMAT = "yyyy-MM-dd hh:mm:ss.SSS";
-    public static final String STRING_DATE  = "2021-01-07 02:02:16.172";
-
-}
